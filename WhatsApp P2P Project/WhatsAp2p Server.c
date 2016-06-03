cf3115f7e5958b5372d7e4d8d435c413d9db62be
Roger Oba <rogerluan.oba@gmail.com>
2016-06-03 14:36:54 -0300

    Created client and server projects.

---
+++ WhatsApp P2P Project/WhatsAp2p Server.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  WhatsAp2p Server
+//
+//  Created by Roger Luan on 6/3/16.
+//  Copyright © 2016 Roger Oba. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
